e86b63d5ee92291078690fec57988c51125f457d
Ted Sanders <tpsjr7@gmail.com>
2022-09-09 20:35:43 -0500

    update run-shell

--- run-shell.sh
+++ run-shell.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
 cd "$(dirname $0)"
-base_path="$(pwd)"
-mkdir -p .cache
-mkdir -p outputs
-cd "$base_path"
-docker run \
-  -v ${base_path}/models:/app/models \
-  -v ${base_path}/img/input:/app/input \
-  -v ${base_path}/run-invert.sh:/app/run-invert.sh \
-  -v ${base_path}/run-inference.sh:/app/run-inference.sh \
-  -v ${base_path}/.cache:/root/.cache \
-  -v /mnt/c/ted/code/img-gen/sd-v1-4.ckpt:/app/models/sd-v1-4.ckpt \
-  -v ${base_path}/logs:/app/logs \
-  -v ${base_path}/outputs:/app/outputs \
-  --gpus 1 -it --rm textual-inversion bash
 
+sudo docker run -it --rm --gpus all \
+  -v $(pwd):/app \
+  -v root-profile:/root \
+  -v conda:/opt/conda/envs \
+  textual-inversion bash
