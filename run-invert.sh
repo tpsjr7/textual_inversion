43411797bcb06b92e26dd3779816b67efdce2f62
Ted Sanders <tpsjr7@gmail.com>
2022-09-09 20:22:01 -0500

    update run invert

--- run-invert.sh
+++ run-invert.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+set -x
 source /root/.bashrc
 conda activate ldm
-export CUDA_VISIBLE_DEVICES=0
 nvidia-smi
+
+if [ "$1" != "" ] ; then
+  gpus="$1"
+else
+  gpus="0,"
+fi
 python main.py --base configs/stable-diffusion/v1-finetune.yaml \
                -t \
-               --actual_resume models/sd-v1-4.ckpt \
+               --actual_resume models/ldm/stable-diffusion-v1/model.ckpt \
                -n chattyrun1 \
-               --gpus 0, \
-               --data_root /app/input \
+               --gpus ${gpus} \
+               --data_root /app/img/input \
                --init_word mascot
